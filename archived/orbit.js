3881a9ff423949d98b72716f65728492383e628f
takasa5 <vipqiv00@gmail.com>
2017-08-09 16:47:38 +0900

    remale with p5.js

---
+++ archived/orbit.js
@@ -0,0 +1,198 @@
+var orbitCount = 0;
+var mouseX, mouseY;
+var downTime, upTime;
+var mouseStatus="createOrbit";
+var playStatus="stop";
+var pointList = [];
+var orbitList = [];
+
+//キャンバスクリック時の挙動 ドラッグと区別のためdown-up
+$("#canvas").on('mousedown', function() {
+  downTime = new Date().getTime();
+});
+$("#canvas").on('mouseup', function(e) {
+  upTime = new Date().getTime();
+  if (upTime - downTime < 250) { //クリック判定
+      if (mouseStatus == "createOrbit") {
+        $('#jquery-ui-dialog').dialog('open');
+        mouseX = e.offsetX;
+        mouseY = e.offsetY;
+      }else if (mouseStatus == "createPlanet") {
+        $("canvas").drawEllipse({
+          strokeStyle: "white",
+          strokeWidth: 0.5,
+          // x,yは近くの円周の5刻みの部分で
+          width:20, height: 20,
+          draggable: true,
+          // updateDragX, Yもx, y同様     
+        })
+      }
+  }
+});
+
+//オービットの作成
+function createOrbit(size) {
+  orbitList.push(orbitCount);
+  var name = "orbit" + orbitCount;
+  $("canvas").drawEllipse({
+    strokeStyle: "white",
+    strokeWidth: 1,
+    x: mouseX,
+    y: mouseY,
+    width: size,
+    height: size,
+    draggable: true,
+    groups: ["orbit" + orbitCount, "orbit"],
+    dragGroups: ["orbit" + orbitCount],
+    name : "orbit" + orbitCount,
+    click: function(layer) {
+      if (mouseStatus == "delete") {
+        $("#canvas").trigger("deleteEvent", [$("canvas").getLayer(layer).groups, $("canvas").getLayer(layer).name]);
+      }
+    },
+    dragstop: function(layer) {
+      var orbit = $("canvas").getLayer(layer);
+      var num = parseInt(orbit.name.replace(/[^0-9^\.]/g,""));
+      pointList[num*2] = orbit.x;
+      pointList[num*2+1] = orbit.y;
+      console.log(pointList);
+    }
+  });
+  pointList[orbitCount*2] = mouseX;
+  pointList[orbitCount*2+1] = mouseY;
+  console.log(pointList);
+  $("canvas").drawEllipse({
+    fillStyle: "white",
+    x: mouseX,
+    y: mouseY,
+    translateY: -size/2,
+    rotate: 0,
+    width: 20,
+    height: 20,
+    draggable: true,
+    groups: ["orbit" + orbitCount, "comets", "orbit"],
+    dragGroups: ["orbit" + orbitCount],
+    name: "comet" + orbitCount,
+    click: function(layer) {
+      if (mouseStatus == "delete") {
+        $("#canvas").trigger("deleteEvent", [$("canvas").getLayer(layer).groups, $("canvas").getLayer(layer).name]);
+      }
+    },
+  })
+  orbitCount++;
+}
+
+//ボタン
+$("#button-planet").on('click', function() {
+  mouseStatus="createPlanet";
+  $("#text-status").text("create planet mode");
+})
+$("#button-orbit").on('click', function() {
+  mouseStatus="createOrbit";
+  $("#text-status").text("create orbit mode");
+})
+$("#button-delete").on('click', function() {
+  mouseStatus="delete";
+  $("#text-status").text("delete mode");
+})
+$("#button-play").on("click", function() {
+  if (playStatus == "play") {
+    playStatus = "pause";
+    $("#canvas").trigger("stopEvent");
+    $("#button-play").attr("src", "images/button-play.png");
+  }else{
+    playStatus="play";
+    $("#canvas").trigger("playEvent");
+    $("#button-play").attr("src", "images/button-pause.png");
+  }
+})
+$("#button-stop").on("click", function() {
+  playStatus="stop";
+  $("#canvas").trigger("stopEvent");
+})
+
+//初期設定的な？
+$(function () {
+  sizing();
+  $(window).resize(function() {
+    sizing();
+  });
+  $('#jquery-ui-slider').slider({
+    value: 100,
+    min: 30,
+    max: 300,
+    slide: function(event, ui) {
+      $('#slider-val').val(ui.value);
+    }
+  });
+  $('#slider-val').val(
+    $('#jquery-ui-slider').slider('value')
+  );
+  $("#jquery-ui-dialog").dialog({
+    modal: true,
+    resizable: false,
+    buttons: true,
+    autoOpen: false,
+  });
+  $('#dialog-button').click(function() {
+    var orbitSize = $("#slider-val").val();
+    createOrbit(orbitSize);
+  })
+});
+
+//リサイズ用
+function sizing() {
+  $("#canvas").attr({height:$("#wrap").height()});
+  $("#canvas").attr({width:$("#wrap").width()});
+}
+
+//デリート
+$("#canvas").on("deleteEvent", function(e, grp, nam) {
+  var name = nam.replace(/[^0-9^\.]/g,"");
+  var num = parseInt(name);
+  var p = orbitList.indexOf(num);
+  orbitList.splice(p, 1);
+  delete pointList[p*2];
+  delete pointList[p*2+1];
+  $("canvas").removeLayerGroup(grp[0]).drawLayers();
+})
+
+//再生関連
+$("#canvas").on("playEvent", function() {
+    setInterval(function() {
+      if (playStatus=="play")
+        $.each(orbitList, function(index, val) {
+          $("canvas").animateLayer("comet" + val, {
+            rotate: '+=5', 
+          }, 1000/72, "linear");
+        }) 
+    }, 1000/72);
+    
+    //window.requestAnimationFrame(revolution);
+})
+
+$("#canvas").on("stopEvent", function() {
+  $.each(orbitList, function(i, val) {
+    $("canvas").stopLayer("comet" + val, true);
+    //console.log(Math.ceil($("canvas").getLayer("comet"+val).rotate % 360));
+    console.log(orbitList);
+    if (playStatus == "stop") {
+      $("canvas").animateLayer("comet" + val, {
+        rotate: 0
+      }, 10, "linear");
+      $("#button-play").attr("src", "images/button-play.png");
+    }
+  })
+})
+
+//animation
+/*
+function revolution() {
+  for (var i = 1; i <= orbitCount; i++) {
+    $("canvas").animateLayer("comet" + i, {
+      rotate: '+=360', 
+    }, 400, "linear");
+  }
+  window.requestAnimationFrame(revolution);
+}
+*/
--- orbit.js
+++ orbit.js
@@ -1,194 +1,190 @@
-var orbitCount = 0;
-var mouseX, mouseY;
-var downTime, upTime;
-var mouseStatus="createOrbit";
-var playStatus="stop";
-var pointList = [];
-var orbitList = [];
+/// <reference path="p5.global-mode.d.ts" />
+orbitnum = 0;
+mode = "createOrbit";
+playmode = "stop";
+orbitList = [];
+myFrame = 0;
+//最初に実行
+function setup() {
+    createCanvas(windowWidth*0.985, windowHeight-100);
+    playButton = createButton("play");
+    playButton.position(windowWidth/3,windowHeight-50);
+    playButton.mousePressed(play);
+    stopButton = createButton("stop");
+    stopButton.position(windowWidth/3 + 60, windowHeight-50);
+    stopButton.mousePressed(stop);
+    orbitButton = createButton("orbit");
+    orbitButton.position(windowWidth/2 + 80, windowHeight-50);
+    orbitButton.mousePressed(function(){mode="createOrbit";});
+    deleteButton = createButton("delete");
+    deleteButton.position(windowWidth/2 + 140, windowHeight-50);
+    deleteButton.mousePressed(function(){mode="delete";});
+    sl = createSlider(10, 400, 100, 10);
+    sl.position(windowWidth/2 - 100,windowHeight-50);
+    sl.style("width", "150px");
+    background(19, 19, 70);
+}
 
-//キャンバスクリック時の挙動 ドラッグと区別のためdown-up
-$("#canvas").on('mousedown', function() {
-  downTime = new Date().getTime();
-});
-$("#canvas").on('mouseup', function(e) {
-  upTime = new Date().getTime();
-  if (upTime - downTime < 250) { //クリック判定
-      if (mouseStatus == "createOrbit") {
-        $('#jquery-ui-dialog').dialog('open');
-        mouseX = e.offsetX;
-        mouseY = e.offsetY;
-      }else if (mouseStatus == "createPlanet") {
-        $("canvas").drawEllipse({
-          strokeStyle: "white",
-          strokeWidth: 0.5,
-          // x,yは近くの円周の5刻みの部分で
-          width:20, height: 20,
-          draggable: true,
-          // updateDragX, Yもx, y同様     
+//常に待機、loop内容
+function draw() {
+    //mode
+    if (mode == "delete") { //削除対象に色付け
+        paintOrbit(0.5, 255, 94, 137);
+    }else{//ドラッグ対象
+        paintOrbit(0.2, 94, 131, 255);
+    }
+    //playmode
+    if (playmode == "play") {
+        revolveOrbit();
+        //動いてるときにも色付け
+        if (mode == "delete") {
+            paintOrbit(0.5, 255, 94, 137);
+        }else{
+            paintOrbit(0.2, 94, 131, 255);
+        }
+        myFrame++;
+    } else {
+
+    }
+
+}
+
+//色付け
+function paintOrbit(range, red, green, blue) {
+    var target = [];
+    orbitList.forEach(function(val, i) {
+        if (mouseX < val.centerX+val.R*range && mouseX > val.centerX-val.R*range &&
+            mouseY < val.centerY+val.R*range && mouseY > val.centerY-val.R*range) {
+            append(target, val);
+        }
+    });
+    revolveOrbit();
+    stroke(red, green, blue); noFill();
+    /*if (only) {
+        var onlytag = target[target.length-1];
+        ellipse(onlytag.centerX, onlytag.centerY, onlytag.R);
+    }else{*/
+        target.forEach(function(val) {
+            ellipse(val.centerX, val.centerY, val.R);
         })
-      }
-  }
-});
-
-//オービットの作成
-function createOrbit(size) {
-  orbitList.push(orbitCount);
-  var name = "orbit" + orbitCount;
-  $("canvas").drawEllipse({
-    strokeStyle: "white",
-    strokeWidth: 1,
-    x: mouseX,
-    y: mouseY,
-    width: size,
-    height: size,
-    draggable: true,
-    groups: ["orbit" + orbitCount, "orbit"],
-    dragGroups: ["orbit" + orbitCount],
-    name : "orbit" + orbitCount,
-    click: function(layer) {
-      if (mouseStatus == "delete") {
-        $("#canvas").trigger("deleteEvent", [$("canvas").getLayer(layer).groups, $("canvas").getLayer(layer).name]);
-      }
-    },
-    dragstop: function(layer) {
-      var orbit = $("canvas").getLayer(layer);
-      var num = parseInt(orbit.name);
-      pointList[num-1] = orbit.x;
-      pointList[num] = orbit.y;
-    }
-  });
-  pointList.push(mouseX);
-  pointList.push(mouseY);
-  $("canvas").drawEllipse({
-    fillStyle: "white",
-    x: mouseX,
-    y: mouseY,
-    translateY: -size/2,
-    rotate: 0,
-    width: 20,
-    height: 20,
-    draggable: true,
-    groups: ["orbit" + orbitCount, "comets", "orbit"],
-    dragGroups: ["orbit" + orbitCount],
-    name: "comet" + orbitCount,
-    click: function(layer) {
-      if (mouseStatus == "delete") {
-        $("#canvas").trigger("deleteEvent", [$("canvas").getLayer(layer).groups, $("canvas").getLayer(layer).name]);
-      }
-    },
-  })
-  orbitCount++;
+    //}
+    fill(255);
+    /*if (only) {
+        var val = target[target.length-1];
+        ellipse(val.centerX + (val.R/2)*cos(myFrame/100*TWO_PI - PI/2), val.centerY + (val.R/2)*sin(myFrame/100*TWO_PI - PI/2), 20);
+    }else{*/
+        target.forEach(function(val) {
+            ellipse(val.centerX + (val.R/2)*cos(myFrame/100*TWO_PI - PI/2), val.centerY + (val.R/2)*sin(myFrame/100*TWO_PI - PI/2), 20);
+        })
+    //}
 }
 
-//ボタン
-$("#button-planet").on('click', function() {
-  mouseStatus="createPlanet";
-  $("#text-status").text("create planet mode");
-})
-$("#button-orbit").on('click', function() {
-  mouseStatus="createOrbit";
-  $("#text-status").text("create orbit mode");
-})
-$("#button-delete").on('click', function() {
-  mouseStatus="delete";
-  $("#text-status").text("delete mode");
-})
-$("#button-play").on("click", function() {
-  if (playStatus == "play") {
-    playStatus = "pause";
-    $("#canvas").trigger("stopEvent");
-    $("#button-play").attr("src", "images/button-play.png");
-  }else{
-    playStatus="play";
-    $("#canvas").trigger("playEvent");
-    $("#button-play").attr("src", "images/button-pause.png");
-  }
-})
-$("#button-stop").on("click", function() {
-  playStatus="stop";
-  $("#canvas").trigger("stopEvent");
-})
-
-//初期設定的な？
-$(function () {
-  sizing();
-  $(window).resize(function() {
-    sizing();
-  });
-  $('#jquery-ui-slider').slider({
-    value: 100,
-    min: 30,
-    max: 300,
-    slide: function(event, ui) {
-      $('#slider-val').val(ui.value);
-    }
-  });
-  $('#slider-val').val(
-    $('#jquery-ui-slider').slider('value')
-  );
-  $("#jquery-ui-dialog").dialog({
-    modal: true,
-    resizable: false,
-    buttons: true,
-    autoOpen: false,
-  });
-  $('#dialog-button').click(function() {
-    var orbitSize = $("#slider-val").val();
-    createOrbit(orbitSize);
-  })
-});
-
-//リサイズ用
-function sizing() {
-  $("#canvas").attr({height:$("#wrap").height()});
-  $("#canvas").attr({width:$("#wrap").width()});
+//ウインドウリサイズ時　！オブジェクト再描画！
+function windowResized() {
+    resizeCanvas(windowWidth*0.985, windowHeight-100);
+    playButton.position(windowWidth/3,windowHeight-50);
+    stopButton.position(windowWidth/3 + 60, windowHeight-50);
+    orbitButton.position(windowWidth/2 + 80, windowHeight-50);
+    deleteButton.position(windowWidth/2 + 140, windowHeight-50);
+    sl.position(windowWidth/2 - 100,windowHeight-50);
+    revolveOrbit();
 }
 
-//デリート
-$("#canvas").on("deleteEvent", function(e, grp, nam) {
-  var name = nam.replace(/[^0-9^\.]/g,"");
-  var num = parseInt(name);
-  var p = orbitList.indexOf(num);
-  orbitList.splice(p, 1);
-  $("canvas").removeLayerGroup(grp[0]).drawLayers();
-})
-
-//再生関連
-$("#canvas").on("playEvent", function() {
-    setInterval(function() {
-      if (playStatus=="play")
-        $.each(orbitList, function(index, val) {
-          $("canvas").animateLayer("comet" + val, {
-            rotate: '+=5', 
-          }, 1000/72, "linear");
-        }) 
-    }, 1000/72);
-    
-    //window.requestAnimationFrame(revolution);
-})
-
-$("#canvas").on("stopEvent", function() {
-  $.each(orbitList, function(i, val) {
-    $("canvas").stopLayer("comet" + val, true);
-    //console.log(Math.ceil($("canvas").getLayer("comet"+val).rotate % 360));
-    console.log(orbitList);
-    if (playStatus == "stop") {
-      $("canvas").animateLayer("comet" + val, {
-        rotate: 0
-      }, 10, "linear");
-      $("#button-play").attr("src", "images/button-play.png");
+//マウスイベント
+function mouseReleased() {
+    clickTime = millis() - pressedTime;
+    if (mouseY < windowHeight-100 && clickTime < 250) {
+        if (mode=="createOrbit") {
+            createOrbit(mouseX, mouseY, sl.value());
+        }else if (mode="delete") {
+            deleteOrbit(mouseX, mouseY);
+        }
     }
-  })
-})
-
-//animation
-/*
-function revolution() {
-  for (var i = 1; i <= orbitCount; i++) {
-    $("canvas").animateLayer("comet" + i, {
-      rotate: '+=360', 
-    }, 400, "linear");
-  }
-  window.requestAnimationFrame(revolution);
 }
-*/
+function mousePressed() {
+    pressedTime = millis();
+    var place = 0;
+    orbitList.forEach(function(val, i) {
+        if (mouseX < val.centerX+val.R*0.2 && mouseX > val.centerX-val.R*0.2 &&
+            mouseY < val.centerY+val.R*0.2 && mouseY > val.centerY-val.R*0.2) {
+            dragTarget = val; place++;
+        }
+    })
+    if (place == 0) 
+        dragTarget = null;
+}
+function mouseDragged() {
+    if (mode!="delete") {
+        dragTarget.centerX = mouseX; dragTarget.centerY = mouseY;
+        revolveOrbit();
+    }
+}
+
+function play() {
+    playmode="play";
+    playButton.remove();
+    playButton = createButton("pause");
+    playButton.position(windowWidth/3, windowHeight-50);
+    playButton.mousePressed(pause);
+}
+function pause() {
+    playmode="pause";
+    playButton.remove();
+    playButton = createButton("play");
+    playButton.position(windowWidth/3, windowHeight-50);
+    playButton.mousePressed(play);
+}
+function stop() {
+    playmode="stop";
+    myFrame = 0;
+    revolveOrbit();
+    playButton.remove();
+    playButton = createButton("play");
+    playButton.position(windowWidth/3, windowHeight-50);
+    playButton.mousePressed(play);
+}
+
+
+function createOrbit(x, y, r) {
+    orbit = {number: orbitnum, centerX: x, centerY: y, R:r};
+    orbitnum++;
+    append(orbitList, orbit);
+    stroke(255); noFill();
+    ellipse(x, y, r);
+    fill(255, 255, 255);
+    ellipse(x, y-r/2, 20);
+    revolveOrbit();
+}
+
+function deleteOrbit(x, y) {
+    var valList = [];
+    orbitList.forEach(function(val, i) {
+        if (x < val.centerX+val.R*0.5 && x > val.centerX-val.R*0.5 &&
+            y < val.centerY+val.R*0.5 && y > val.centerY-val.R*0.5) 
+                append(valList, val);
+    });
+    valList.forEach(function(val, i) {
+        orbitList.splice(orbitList.indexOf(val), 1);
+    });
+    revolveOrbit();
+}
+
+function revolveOrbit() {
+    background(19, 19, 70);
+    orbitList.forEach(function(val, i) {
+        stroke(255); noFill();
+        ellipse(val.centerX, val.centerY, val.R);
+        fill(255, 255, 255);
+        ellipse(val.centerX + (val.R/2)*cos(myFrame/100*TWO_PI - PI/2), val.centerY + (val.R/2)*sin(myFrame/100*TWO_PI - PI/2), 20);
+    });
+}
+
+function redrawOrbit() {
+    background(19, 19, 70);
+    orbitList.forEach(function(val, i) {
+        stroke(255); noFill();
+        ellipse(val.centerX, val.centerY, val.R);
+        fill(255, 255, 255);
+        ellipse(val.centerX, val.centerY-val.R/2, 20);
+    });
+}
